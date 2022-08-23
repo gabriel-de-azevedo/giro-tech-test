bc557f1e0fdd9f37bcb7e8d673da1f22b4b5c401
Gabriel de Azevedo <gmarquesazti@gmail.com>
2022-08-23 14:08:27 -0300

    feat: add login handling

--- src/Components/LoginForm/index.js
+++ src/Components/LoginForm/index.js
@@ -5,9 +5,46 @@ import {
   FormLabel,
   Input,
   Stack,
+  useToast,
 } from '@chakra-ui/react';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { validateLogin } from '../../database/controllers';
+
 export const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (validateLogin(email, password)) {
+      toast({
+        title: 'Bem-vindo',
+        description: 'Que bom ver você novamente',
+        status: 'success',
+        position: 'top-right',
+        duration: 2000,
+        isClosable: true,
+      });
+
+      return navigate(`/simulation/${email}`);
+    }
+
+    toast({
+      title: 'Tente novamente',
+      description: 'Credenciais incorretas',
+      status: 'error',
+      position: 'top-right',
+      duration: 2000,
+      isClosable: true,
+    });
+  };
+
   return (
     <FormControl
       isRequired
@@ -17,17 +54,27 @@ export const LoginForm = () => {
       p={4}
       shadow="md"
     >
-      <Stack spacing={4}>
+      <Stack spacing={4} as="form" onSubmit={handleLogin}>
         <Box>
           <FormLabel>Email</FormLabel>
-          <Input type="email" />
+          <Input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Box>
         <Box>
           <FormLabel>Senha</FormLabel>
-          <Input type="password" />
+          <Input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Box>
         <Box />
-        <Button>Login</Button>
+        <Button type="submit">Login</Button>
       </Stack>
     </FormControl>
   );
